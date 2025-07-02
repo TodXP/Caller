fb3865b900be7ac90cee8edf462af482eba81b53
TodXP <todxd@thijmentiscool.com>
2025-07-02 17:55:48 +0200

    Created a class

---
+++ src/server/main_server.py
@@ -0,0 +1,20 @@
+import socket
+import threading
+
+# Making a list for the clients to connect to.
+
+clients = {}
+
+
+# Creating server class
+class Server():
+    def __init__(self):
+        pass
+
+    def start_server(self):
+        print("Server started")
+
+
+if __name__ == "__main__":
+    server = Server()
+    server.start_server()
