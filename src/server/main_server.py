df2fdab767aa25aedae27837eb765956d17075cc
TodXP <todxd@thijmentiscool.com>
2025-07-02 18:04:16 +0200

    Made it so you can publicly create a port.

--- src/server/main_server.py
+++ src/server/main_server.py
@@ -9,9 +9,16 @@ clients = {}
 # Creating server class
 class Server():
     def __init__(self):
-        pass
+        self.hostip = "127.0.0.1" # Default localhost
+        self.hostport = 5555
 
     def start_server(self):
+        #Creating a host
+        server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server_socket.bind((self.hostip, self.hostport))
+        print(f"{self.hostip}:{self.hostport}")
+
+
         print("Server started")
 
 
