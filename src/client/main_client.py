1b4fb2ab1224847a2ff95bb261351dac4e47bd20
TodXP <todxd@thijmentiscool.com>
2025-07-02 18:24:38 +0200

    Created a client class

--- src/client/main_client.py
+++ src/client/main_client.py
@@ -1,19 +1,9 @@
-import os
+import socket
 
 
-own_number = 1
+class Client():
+    def __init__(self):
+        pass
 
-os.system("clear")
-
-
-while True:
-    
-    ntc = int(input("Enter the number you want to call: "))
-
-    if ntc != own_number:
-        match ntc:
-            case 1:
-                print("Calling TOD")
-            case 2:
-                print("Calling cops")
-    else: print("Calling yourself is kind of difficult")
+if __name__ == "__main__":
+    client = Client()
